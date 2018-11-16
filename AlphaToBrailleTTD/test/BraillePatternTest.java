21195f7ce9f5e01bff1e91c8d432cb8fa54be6b7
Seif Adil Fadel <sfa047@post.uit.no>
2018-11-16 10:50:22 +0100

    JunitTest Class

---
+++ AlphaToBrailleTTD/test/BraillePatternTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import java.util.Arrays;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.Ignore;
+
+public class BraillePatternTest {
+
+
+
+}
